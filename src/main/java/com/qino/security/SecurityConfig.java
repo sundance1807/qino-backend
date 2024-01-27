952d214360244bc1c4b08937bef32092d60c856d
Askar.A <aalyiev7@gmail.com>
2024-01-27 21:23:59 +0600

    implemented userDetailService and UserDetails

--- src/main/java/com/qino/security/SecurityConfig.java
+++ src/main/java/com/qino/security/SecurityConfig.java
@@ -1,38 +1,46 @@
-//package com.qino.security;
-//
-//import lombok.Getter;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@Getter
-//@EnableWebSecurity
-//public abstract class SecurityConfig extends WebSecurityConfiguration {
-//
-//    private final CustomUserDetailService customUserDetailService;
-//
-//
-//    @Autowired
-//    public SecurityConfig(CustomUserDetailService customUserDetailService) {
-//        this.customUserDetailService = customUserDetailService;
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests((authorize) -> authorize
-//                        .requestMatchers("/api/auth/**").permitAll()
-//                        .anyRequest().permitAll());
-//
-//        return http.build();
-//    }
-//}
+package com.qino.security;
+
+import lombok.Getter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@Getter
+@EnableWebSecurity
+public abstract class SecurityConfig extends WebSecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(HttpMethod.GET).permitAll()
+                        .anyRequest().permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("admin")
+                .roles("admin")
+                .build();
+        UserDetails user = User.builder()
+                .username("user")
+                .password("user")
+                .roles("user")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+}
