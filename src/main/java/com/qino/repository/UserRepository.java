41f40d5018da01a6ae5c97199ac5df78f5cfb51c
Askar.A <aalyiev7@gmail.com>
2024-01-27 21:43:32 +0600

    implemented UserDetailService and AuthenticationManager

--- src/main/java/com/qino/repository/UserRepository.java
+++ src/main/java/com/qino/repository/UserRepository.java
@@ -1,7 +1,5 @@
 package com.qino.repository;
 
-import com.qino.exception.CustomException;
-import com.qino.model.dto.UserDto;
 import com.qino.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,5 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUsername(String username);
+
     Boolean existsByUsername(String username);
 }
--- src/main/java/com/qino/security/SecurityConfig.java
+++ src/main/java/com/qino/security/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.qino.security;
 
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
@@ -11,6 +14,8 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -18,12 +23,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @Getter
 @EnableWebSecurity
 public abstract class SecurityConfig extends WebSecurityConfiguration {
+
+    private final CustomUserDetailService userDetailService;
+    @Autowired
+    public SecurityConfig(CustomUserDetailService userDetailService) {
+        this.userDetailService = userDetailService;
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers(HttpMethod.GET).permitAll()
+                        .requestMatchers(HttpMethod.GET).authenticated()
                         .anyRequest().permitAll());
 
         return http.build();
@@ -42,5 +53,16 @@ public abstract class SecurityConfig extends WebSecurityConfiguration {
                 .roles("user")
                 .build();
         return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
