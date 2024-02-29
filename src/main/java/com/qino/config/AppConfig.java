c1c07835ab0018e366f84b2a3d727fa19bbcb7a6
Askar.A <aalyiev7@gmail.com>
2024-02-29 11:11:39 +0600

    added generator bean

--- src/main/java/com/qino/config/AppConfig.java
+++ src/main/java/com/qino/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.qino.config;
 
+import com.qino.util.Generator;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
@@ -12,5 +13,10 @@ public class AppConfig {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         return modelMapper;
+    }
+
+    @Bean
+    public Generator generator() {
+        return new Generator();
     }
 }
