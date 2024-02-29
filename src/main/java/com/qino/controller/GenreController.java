9a35d592676d4613b13605ffd682f2afc97ff40d
Askar.A <aalyiev7@gmail.com>
2024-02-29 15:57:08 +0600

    api position fix

--- src/main/java/com/qino/controller/GenreController.java
+++ src/main/java/com/qino/controller/GenreController.java
@@ -17,44 +17,45 @@ import java.util.Set;
 public class GenreController {
     private GenreService genreService;
 
-    @PostMapping
+    @PostMapping("/genre")
     @ResponseStatus(HttpStatus.CREATED)
     public GenreDTO saveOne(@RequestBody GenreDTO genreDTO) throws CustomException {
         log.info("Incoming request to create a genre: {}.", genreDTO.getName());
         return genreService.saveOne(genreDTO);
     }
 
-    @ResponseStatus(HttpStatus.FOUND)
-    @GetMapping("/{id}")
-    public GenreDTO getOne(@PathVariable ("id") Long id) throws CustomException {
-        log.info("Incoming request to get a genre with id: {}.", id);
-        return genreService.findOne(id);
-    }
-
-    @ResponseStatus(HttpStatus.OK)
-    @PutMapping("/{id}")
-    public GenreDTO updateOne(@PathVariable ("id") Long id, @RequestBody GenreDTO genreDTO) throws CustomException {
-        log.info("Incoming request to update '{}' genre.", genreDTO.getName());
-        return genreService.updateOne(id, genreDTO);
-    }
-
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping("/{id}")
-    public void deleteOne(@PathVariable ("id") Long id) throws CustomException {
-        log.info("Incoming request to delete a genre with id: {}.",id);
-        genreService.deleteOne(id);
-    }
-
-    @PostMapping("/createAll")
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Set<GenreDTO> saveAll(@RequestBody Set<GenreDTO> genreDTOSet) throws CustomException {
         log.info("Incoming request to create a genres: {}.", genreDTOSet);
         return genreService.saveAll(genreDTOSet);
     }
 
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public GenreDTO getOne(@PathVariable ("id") Long id) throws CustomException {
+        log.info("Incoming request to get a genre with id: {}.", id);
+        return genreService.findOne(id);
+    }
+
     @GetMapping("/showAll")
+    @ResponseStatus(HttpStatus.FOUND)
     public Set<GenreDTO> getAll() {
         log.info("Incoming request to get all genres.");
         return genreService.findAll();
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public GenreDTO updateOne(@PathVariable ("id") Long id, @RequestBody GenreDTO genreDTO) throws CustomException {
+        log.info("Incoming request to update '{}' genre.", genreDTO.getName());
+        return genreService.updateOne(id, genreDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteOne(@PathVariable ("id") Long id) throws CustomException {
+        log.info("Incoming request to delete a genre with id: {}.",id);
+        genreService.deleteOne(id);
     }
 }
---
+++ src/main/java/com/qino/model/dto/FilmDTO.java
@@ -0,0 +1,14 @@
+//package com.qino.model.dto;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.EqualsAndHashCode;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@EqualsAndHashCode(callSuper = true)
+//public class FilmDTO extends BaseDTO{
+//
+//}
