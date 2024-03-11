f8de94cebd3a2a2ca5043ef5749cbc94531c0ca8
Askar.A <aalyiev7@gmail.com>
2024-03-11 12:01:46 +0500

    deleted genres.sql

--- src/main/resources/inserts/genres.sql
+++
@@ -1,7 +0,0 @@
-insert into genres (id, created_at, created_by, name)
-values
-(1, '2024-03-07 12:00:00', 'SYSTEM', 'Anime'),
-(2, '2024-03-07 12:00:00', 'SYSTEM', 'Action'),
-(3, '2024-03-07 12:00:00', 'SYSTEM', 'Drama'),
-(4, '2024-03-07 12:00:00', 'SYSTEM', 'Sport'),
-(5, '2024-03-07 12:00:00', 'SYSTEM', 'Thriller');
